52bb1f1934fb474ed73434b0c26e64a1d97777f9
Igor Luiz <79776778+IgorLuiz777@users.noreply.github.com>
2023-11-08 11:23:02 -0300

    home

--- sprint04_front/src/app/page.js
+++ sprint04_front/src/app/page.js
@@ -1,9 +1,23 @@
 
+import Button from "@/components/button/button"
+import './home.scss'
 
 export default function Home() {
-  return (
+  return(
     <main>
-      <h1>Home</h1>
+        <section className="part1">
+            <div className="div1">  
+                <h1 className="titulo1">Não corra riscos com a segurança da sua bicicleta. Proteja seu patrimônio com nosso seguro completo para bicicletas.</h1>
+                <p className="texto1">Faça agora mesmo o cadastramento de sua bicicleta clicando no botão abaixo:</p>
+                <Button href="/cadastro">Cadastre-se agora</Button>
+            </div>
+                
+            <div className="divimg">
+                <img id="img1" src="bike2.webp" alt="Bike"/>
+            </div>
+                
+        </section>
+            
     </main>
-  )
+)
 }
--- sprint04_front/src/components/button/button.jsx
+++ sprint04_front/src/components/button/button.jsx
@@ -1,7 +1,8 @@
 
 import Link from "next/link"
+import './button.scss'
 
-export default function Button () {
+export default function Button (props) {
 
     return(
         <div className="button">
