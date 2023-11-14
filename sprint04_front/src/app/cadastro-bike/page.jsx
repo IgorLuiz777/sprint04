aed4eb0b79b6379c2c6bfe258cea4087428512f6
Igor Luiz <79776778+IgorLuiz777@users.noreply.github.com>
2023-11-13 21:43:29 -0300

    front_finalizado

--- sprint04_front/src/app/cadastro-bike/page.jsx
+++ sprint04_front/src/app/cadastro-bike/page.jsx
@@ -1,24 +1,46 @@
 
-import './page.scss'
+"use client"
+import { useState } from 'react';
+import './page.scss';
 
-export default function CadastroBike () {
+export default function CadastroBike() {
+  const [selectedImages, setSelectedImages] = useState([]);
+  
+  const handleImageChange = (e) => {
+    const files = e.target.files;
+    const imageArray = Array.from(files);
 
-    return(
-        <main>
-            <section className="content-cadastro-bike">
-                <h2>Capture imagens da sua bicicleta inspirando-se nos exemplos abaixo:</h2>
+    setSelectedImages(imageArray);
+  };
 
-                <p>Tire fotos da sua bicicleta em pé, em um ambiente bem iluminado, com o celular na horizontal.</p>
+  const handleImageUpload = () => {
+    const imageNames = selectedImages.map((image) => image.name);
+    localStorage.setItem('uploadedImages', JSON.stringify(imageNames));
 
-                <img src="bike-ex1.jpg" alt="exemplo bike"/>
-                <img src="bike-ex2.jpg" alt="exemplo bike"/>
+    console.log('Imagens enviadas com sucesso!');
+  };
 
-                <div className="upload">
-                    <h2>Faça o upload de imagens da sua bicicleta:</h2>
-                    <input type="file" accept="image/*" multiple />
-                    <button>Enviar Imagens</button>
-                </div>
-            </section> 
-        </main>
-    )
+  return (
+    <main>
+      <section className="content-cadastro-bike">
+        <h2>Capture imagens da sua bicicleta inspirando-se nos exemplos abaixo:</h2>
+
+        <p>Tire fotos da sua bicicleta em pé, em um ambiente bem iluminado, com o celular na horizontal.</p>
+
+        <img src="bike-ex1.jpg" alt="exemplo bike" />
+        <img src="bike-ex2.jpg" alt="exemplo bike" />
+
+        <div className="upload">
+          <h2>Faça o upload de imagens da sua bicicleta:</h2>
+          <input
+            type="file"
+            accept="image/*"
+            multiple
+            onChange={handleImageChange}
+          />
+          <button onClick={handleImageUpload}>Enviar Imagens</button>
+        </div>
+      </section>
+    </main>
+  );
 }
--- sprint04_front/src/app/login/page.jsx
+++ sprint04_front/src/app/login/page.jsx
@@ -3,37 +3,25 @@
 import { useEffect, useState } from 'react';
 import './page.scss';
 
-
 export default function Login() {
-    const [cpf, setCpf] = useState('');
-    
-    function formataCPF(e) {
-        const cpfValue = e.target.value;
-        const formattedCPF = cpfValue.replace(
-            /(\d{3})(\d{3})(\d{3})(\d{2})/,
-            (match, part1, part2, part3, part4) => `${part1}.${part2}.${part3}-${part4}`
-        );
-        setCpf(formattedCPF);
-    }
+    const [email, setEmail] = useState('');
 
     return (
         <main>
             <section className="content">
                 <h2>Login</h2>
                 <form>
-
                     <div className="form">
-                        <label htmlFor="cpf">CPF:</label>
+                        <label htmlFor="email">Email:</label>
                         <input
-                            type="text"
-                            name="cpf"
-                            placeholder="Digite seu CPF"
-                            pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"
-                            title="Digite um CPF no formato: xxx.xxx.xxx-xx"
-                            value={cpf}
-                            onChange={formataCPF}
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Digite seu email"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
-                        <input type="submit" value="Verificar" required />
                     </div>
 
                     <div className="form">
@@ -46,8 +34,8 @@ export default function Login() {
                             required
                         />
                     </div>
-                    
-                    <button>enviar</button>
+
+                    <button>Enviar</button>
                 </form>
             </section>
         </main>
